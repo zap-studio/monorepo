e0c423bfdb31b32cad66fe3e43ffedebf5d73372
Alexandre Trotel <trotelalexandre@proton.me>
2025-03-18 14:20:48 +0100

    feat: refactor email sending logic and create resend utility

--- src/actions/emails.action.ts
+++ src/actions/emails.action.ts
@@ -1,11 +1,9 @@
 "use server";
 
-import { Resend } from "resend";
+import { resend } from "@/lib/resend";
 import EmailTemplate from "@/components/emails/template";
 
-const resend = new Resend(process.env.RESEND_API_KEY!);
-
-// TODO: change the settings below
+// TODO: change the settings below especially the from email address
 export const sendMail = async (subject: string, recipients: string[]) => {
   const { data, error } = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
