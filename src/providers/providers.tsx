c2bf39ebfdda494ed9b48e03f15b68b223041c9e
Alexandre Trotel <trotelalexandre@proton.me>
2025-03-20 22:46:02 +0100

    docs: update README for clarity and modernize content

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -2,9 +2,9 @@
 
 // ZAP_PLUGIN:pwa:START
 import { usePushNotificationsStore } from "../stores/push-notifications.store";
+import { useEffect } from "react";
 // ZAP_PLUGIN:pwa:END
 import { ThemeProvider } from "./theme.provider";
-import { useEffect } from "react";
 
 interface ProvidersProps {
   children: React.ReactNode;
