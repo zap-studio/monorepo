debd1e93662c1d95bcdde4dce682bd6ecf806a3b
Alexandre Trotel <trotelalexandre@proton.me>
2025-03-20 22:59:03 +0100

    feat: remove unnecessary dependencies from legal plugin metadata

--- zap/plugins/blog/index.ts
+++ zap/plugins/blog/index.ts
@@ -2,6 +2,5 @@ import { PluginMetadata } from "../../schemas/plugins.schema";
 
 export const blogPlugin: PluginMetadata = {
   name: "blog",
-  dependencies: ["@mdx-js/react", "@mdx-js/loader", "@next/mdx", "@types/mdx"],
   available: false,
 };
--- zap/plugins/legal/index.ts
+++ zap/plugins/legal/index.ts
@@ -2,6 +2,5 @@ import { PluginMetadata } from "../../schemas/plugins.schema";
 
 export const legalPlugin: PluginMetadata = {
   name: "legal",
-  dependencies: ["@mdx-js/react", "@mdx-js/loader", "@next/mdx", "@types/mdx"],
   available: false,
 };
