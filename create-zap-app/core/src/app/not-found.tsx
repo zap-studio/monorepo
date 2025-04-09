8a15c001247e64a9404310f4e724a13b94334b55
Alexandre Trotel <trotelalexandre@proton.me>
2025-04-09 19:40:28 +0200

    feat: update NotFoundPage component with improved styling and button integration

--- create-zap-app/core/src/app/not-found.tsx
+++ create-zap-app/core/src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function NotFoundPage() {
@@ -5,19 +6,16 @@ export default function NotFoundPage() {
     <div className="flex min-h-screen items-center px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
       <div className="w-full space-y-6 text-center">
         <div className="space-y-3">
-          <h1 className="animate-bounce text-4xl font-bold tracking-tighter sm:text-5xl">
+          <h1 className="text-primary animate-bounce text-4xl font-bold tracking-tighter sm:text-5xl">
             404
           </h1>
-          <p className="text-gray-500">
+          <p className="text-muted-foreground">
             Looks like you&apos;ve found the edge case...{" "}
           </p>
         </div>
-        <Link
-          href="/"
-          className="inline-flex h-10 items-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:ring-1 focus-visible:ring-gray-950 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-        >
-          Return to website
-        </Link>
+        <Button asChild>
+          <Link href="/">Return to website</Link>
+        </Button>
       </div>
     </div>
   );
