a93ff59c9ad95116b9e58e07fe42b13b34a3e72c
Alexandre Trotel <trotelalexandre@proton.me>
2025-04-14 13:36:46 +0200

    chore: update ws package to version 8.18.1 and configure neonConfig to use ws as the WebSocket constructor

--- create-zap-app/core/src/db/index.ts
+++ create-zap-app/core/src/db/index.ts
@@ -1,6 +1,9 @@
 import * as schema from "./schema";
-import { neon } from "@neondatabase/serverless";
+import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql, schema });
