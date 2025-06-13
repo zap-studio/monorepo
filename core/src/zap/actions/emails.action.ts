8b039ae5ddb1aaa6451ce0dbf7222ab4e0dd6f6b
Alexandre Trotel <trotelalexandre@proton.me>
2025-06-13 19:05:03 +0200

    fix: make email config dynamic

--- core/src/zap/actions/emails.action.ts
+++ core/src/zap/actions/emails.action.ts
@@ -3,141 +3,142 @@
 import { Effect } from "effect";
 import { JSX } from "react";
 
+import { ZAP_DEFAULT_SETTINGS } from "@/zap.config";
 import { ForgotPasswordEmail } from "@/zap/components/emails/forgot-password";
 import { MagicLinkEmail } from "@/zap/components/emails/magic-link";
 import { VerificationEmail } from "@/zap/components/emails/verification";
 import { resend } from "@/zap/lib/resend/server";
 
-const from = "Zap.ts <hello@mail.alexandretrotel.org>";
+const from = ZAP_DEFAULT_SETTINGS.MAIL.FROM;
 
 interface ForgotPasswordEmailProps {
-  subject: string;
-  recipients: string[];
-  url: string;
+	subject: string;
+	recipients: string[];
+	url: string;
 }
 
 export const sendForgotPasswordMail = async ({
-  subject,
-  recipients,
-  url,
+	subject,
+	recipients,
+	url,
 }: ForgotPasswordEmailProps) => {
-  return Effect.runPromise(
-    Effect.gen(function* (_) {
-      const { data, error } = yield* _(
-        Effect.tryPromise({
-          try: () =>
-            resend.emails.send({
-              from,
-              to: recipients,
-              subject,
-              react: ForgotPasswordEmail({ url }),
-            }),
-          catch: (e) => e,
-        }),
-      );
+	return Effect.runPromise(
+		Effect.gen(function* (_) {
+			const { data, error } = yield* _(
+				Effect.tryPromise({
+					try: () =>
+						resend.emails.send({
+							from,
+							to: recipients,
+							subject,
+							react: ForgotPasswordEmail({ url }),
+						}),
+					catch: (e) => e,
+				}),
+			);
 
-      if (error) {
-        return yield* _(Effect.fail(error));
-      }
+			if (error) {
+				return yield* _(Effect.fail(error));
+			}
 
-      return data;
-    }),
-  );
+			return data;
+		}),
+	);
 };
 
 export const sendVerificationEmail = async ({
-  subject,
-  recipients,
-  url,
+	subject,
+	recipients,
+	url,
 }: ForgotPasswordEmailProps) => {
-  return Effect.runPromise(
-    Effect.gen(function* (_) {
-      const { data, error } = yield* _(
-        Effect.tryPromise({
-          try: () =>
-            resend.emails.send({
-              from,
-              to: recipients,
-              subject,
-              react: VerificationEmail({ url }),
-            }),
-          catch: (e) => e,
-        }),
-      );
+	return Effect.runPromise(
+		Effect.gen(function* (_) {
+			const { data, error } = yield* _(
+				Effect.tryPromise({
+					try: () =>
+						resend.emails.send({
+							from,
+							to: recipients,
+							subject,
+							react: VerificationEmail({ url }),
+						}),
+					catch: (e) => e,
+				}),
+			);
 
-      if (error) {
-        return yield* _(Effect.fail(error));
-      }
+			if (error) {
+				return yield* _(Effect.fail(error));
+			}
 
-      return data;
-    }),
-  );
+			return data;
+		}),
+	);
 };
 
 interface SendMailProps {
-  subject: string;
-  recipients: string[];
-  react?: JSX.Element;
+	subject: string;
+	recipients: string[];
+	react?: JSX.Element;
 }
 
 export const sendMail = async ({
-  subject,
-  recipients,
-  react,
+	subject,
+	recipients,
+	react,
 }: SendMailProps) => {
-  return Effect.runPromise(
-    Effect.gen(function* (_) {
-      const { data, error } = yield* _(
-        Effect.tryPromise({
-          try: () =>
-            resend.emails.send({
-              from,
-              to: recipients,
-              subject,
-              react,
-            }),
-          catch: (e) => e,
-        }),
-      );
+	return Effect.runPromise(
+		Effect.gen(function* (_) {
+			const { data, error } = yield* _(
+				Effect.tryPromise({
+					try: () =>
+						resend.emails.send({
+							from,
+							to: recipients,
+							subject,
+							react,
+						}),
+					catch: (e) => e,
+				}),
+			);
 
-      if (error) {
-        return yield* _(Effect.fail(error));
-      }
+			if (error) {
+				return yield* _(Effect.fail(error));
+			}
 
-      return data;
-    }),
-  );
+			return data;
+		}),
+	);
 };
 
 export const sendMagicLinkEmail = async ({
-  subject,
-  recipients,
-  url,
+	subject,
+	recipients,
+	url,
 }: {
-  subject: string;
-  recipients: string[];
-  url: string;
+	subject: string;
+	recipients: string[];
+	url: string;
 }) => {
-  return Effect.runPromise(
-    Effect.gen(function* (_) {
-      const { data, error } = yield* _(
-        Effect.tryPromise({
-          try: () =>
-            resend.emails.send({
-              from,
-              to: recipients,
-              subject,
-              react: MagicLinkEmail({ url }),
-            }),
-          catch: (e) => e,
-        }),
-      );
+	return Effect.runPromise(
+		Effect.gen(function* (_) {
+			const { data, error } = yield* _(
+				Effect.tryPromise({
+					try: () =>
+						resend.emails.send({
+							from,
+							to: recipients,
+							subject,
+							react: MagicLinkEmail({ url }),
+						}),
+					catch: (e) => e,
+				}),
+			);
 
-      if (error) {
-        return yield* _(Effect.fail(error));
-      }
+			if (error) {
+				return yield* _(Effect.fail(error));
+			}
 
-      return data;
-    }),
-  );
+			return data;
+		}),
+	);
 };
--- core/zap.config.ts
+++ core/zap.config.ts
@@ -11,140 +11,142 @@ export const DEV = process.env.NODE_ENV !== "production";
 
 export const APP_NAME = "Zap.ts";
 export const APP_DESCRIPTION =
-  "The boilerplate to build application as fast as a zap.";
+	"The boilerplate to build application as fast as a zap.";
+export const MAIL = "hello@mail.alexandretrotel.org";
 export const BASE_URL = DEV
-  ? "http://localhost:3000"
-  : "https://demo.zap-ts.alexandretrotel.org";
+	? "http://localhost:3000"
+	: "https://demo.zap-ts.alexandretrotel.org";
 
 export const ZAP_DEFAULT_FLAGS = {
-  VERCEL: {
-    ENABLE_ANALYTICS: VERCEL,
-    ENABLE_SPEED_INSIGHTS: VERCEL,
-  },
-  ENABLE_POSTHOG: false,
+	VERCEL: {
+		ENABLE_ANALYTICS: VERCEL,
+		ENABLE_SPEED_INSIGHTS: VERCEL,
+	},
+	ENABLE_POSTHOG: false,
 };
 
 const AI_SYSTEM_PROMPT = "You are a helpful assistant.";
 
 export const ZAP_DEFAULT_SETTINGS: ZapSettings = {
-  AI: {
-    SYSTEM_PROMPT: AI_SYSTEM_PROMPT,
-  },
-  AUTH: {
-    REQUIRE_EMAIL_VERIFICATION: true,
-    ENABLE_SOCIAL_PROVIDER: true,
-    MINIMUM_USERNAME_LENGTH: 3,
-    MAXIMUM_USERNAME_LENGTH: 20,
-    MINIMUM_PASSWORD_LENGTH: 8,
-    MAXIMUM_PASSWORD_LENGTH: 128,
-    REDIRECT_URL_AFTER_SIGN_UP: "/login",
-    REDIRECT_URL_AFTER_SIGN_IN: "/app",
-  },
-  NOTIFICATIONS: {
-    VAPID_MAIL: "hello@mail.alexandretrotel.org",
-  },
-  MAIL: {
-    PREFIX: APP_NAME,
-    RATE_LIMIT_SECONDS: 60,
-  },
-  PWA: {
-    NAME: APP_NAME,
-    SHORT_NAME: APP_NAME,
-    DESCRIPTION: APP_DESCRIPTION,
-    START_URL: "/",
-    BACKGROUND_COLOR: "#ffffff",
-    THEME_COLOR: "#000000",
-    ICONS: [
-      {
-        src: "/icon-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  },
+	AI: {
+		SYSTEM_PROMPT: AI_SYSTEM_PROMPT,
+	},
+	AUTH: {
+		REQUIRE_EMAIL_VERIFICATION: true,
+		ENABLE_SOCIAL_PROVIDER: true,
+		MINIMUM_USERNAME_LENGTH: 3,
+		MAXIMUM_USERNAME_LENGTH: 20,
+		MINIMUM_PASSWORD_LENGTH: 8,
+		MAXIMUM_PASSWORD_LENGTH: 128,
+		REDIRECT_URL_AFTER_SIGN_UP: "/login",
+		REDIRECT_URL_AFTER_SIGN_IN: "/app",
+	},
+	NOTIFICATIONS: {
+		VAPID_MAIL: MAIL,
+	},
+	MAIL: {
+		PREFIX: APP_NAME,
+		RATE_LIMIT_SECONDS: 60,
+		FROM: `${APP_NAME} <${MAIL}>`,
+	},
+	PWA: {
+		NAME: APP_NAME,
+		SHORT_NAME: APP_NAME,
+		DESCRIPTION: APP_DESCRIPTION,
+		START_URL: "/",
+		BACKGROUND_COLOR: "#ffffff",
+		THEME_COLOR: "#000000",
+		ICONS: [
+			{
+				src: "/icon-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/icon-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+		],
+	},
 };
 
 export const ZAP_DEFAULT_METADATA: Metadata = {
-  title: APP_NAME,
-  description: APP_DESCRIPTION,
-  category: "technology",
-  generator: "Next.js",
-  applicationName: APP_NAME,
-  referrer: "origin-when-cross-origin",
-  keywords: [
-    "Zap.ts",
-    "typescript",
-    "nextjs",
-    "react",
-    "boilerplate",
-    "template",
-    "web",
-    "application",
-  ],
-  authors: [
-    {
-      name: "Alexandre Trotel",
-      url: "https://www.alexandretrotel.org",
-    },
-  ],
-  creator: "Alexandre Trotel",
-  publisher: "Alexandre Trotel",
-  metadataBase: new URL(BASE_URL),
-  openGraph: {
-    title: APP_NAME,
-    description: APP_DESCRIPTION,
-    url: BASE_URL,
-    siteName: APP_NAME,
-    images: [
-      {
-        url: `${BASE_URL}/og.png`,
-        width: 1200,
-        height: 630,
-        alt: `${APP_NAME} Open Graph Image`,
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: false,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: false,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  icons: {
-    icon: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
-    shortcut: "/favicon.ico",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: APP_NAME,
-    description: APP_DESCRIPTION,
-    creator: "@alexandretrotel",
-    images: [`${BASE_URL}/og.png`],
-  },
-  appleWebApp: {
-    title: APP_NAME,
-    statusBarStyle: "black-translucent",
-    capable: true,
-  },
-  appLinks: {
-    web: {
-      url: "https://demo.zap-ts.alexandretrotel.org",
-      should_fallback: true,
-    },
-  },
+	title: APP_NAME,
+	description: APP_DESCRIPTION,
+	category: "technology",
+	generator: "Next.js",
+	applicationName: APP_NAME,
+	referrer: "origin-when-cross-origin",
+	keywords: [
+		"Zap.ts",
+		"typescript",
+		"nextjs",
+		"react",
+		"boilerplate",
+		"template",
+		"web",
+		"application",
+	],
+	authors: [
+		{
+			name: "Alexandre Trotel",
+			url: "https://www.alexandretrotel.org",
+		},
+	],
+	creator: "Alexandre Trotel",
+	publisher: "Alexandre Trotel",
+	metadataBase: new URL(BASE_URL),
+	openGraph: {
+		title: APP_NAME,
+		description: APP_DESCRIPTION,
+		url: BASE_URL,
+		siteName: APP_NAME,
+		images: [
+			{
+				url: `${BASE_URL}/og.png`,
+				width: 1200,
+				height: 630,
+				alt: `${APP_NAME} Open Graph Image`,
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		nocache: false,
+		googleBot: {
+			index: true,
+			follow: true,
+			noimageindex: false,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	icons: {
+		icon: "/favicon.ico",
+		apple: "/apple-touch-icon.png",
+		shortcut: "/favicon.ico",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: APP_NAME,
+		description: APP_DESCRIPTION,
+		creator: "@alexandretrotel",
+		images: [`${BASE_URL}/og.png`],
+	},
+	appleWebApp: {
+		title: APP_NAME,
+		statusBarStyle: "black-translucent",
+		capable: true,
+	},
+	appLinks: {
+		web: {
+			url: "https://demo.zap-ts.alexandretrotel.org",
+			should_fallback: true,
+		},
+	},
 };
