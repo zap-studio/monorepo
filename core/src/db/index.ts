fa26eb71463ef17cc70c928bce0e6a9442c38b58
Alexandre Trotel <trotelalexandre@proton.me>
2025-06-14 01:03:15 +0200

    fix: change throw error if database url is not defined

--- core/src/db/index.ts
+++ core/src/db/index.ts
@@ -3,5 +3,9 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+const sql = neon(databaseUrl);
 export const db = drizzle({ client: sql, schema });
