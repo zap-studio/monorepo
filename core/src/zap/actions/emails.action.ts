f30dd1573ee94436db1b0ffe8e812759ae705be8
Alexandre Trotel <trotelalexandre@proton.me>
2025-06-13 23:29:01 +0200

    fix: lint and format

--- core/src/zap/actions/emails.action.ts
+++ core/src/zap/actions/emails.action.ts
@@ -12,133 +12,133 @@ import { resend } from "@/zap/lib/resend/server";
 const from = ZAP_DEFAULT_SETTINGS.MAIL.FROM;
 
 interface ForgotPasswordEmailProps {
-	subject: string;
-	recipients: string[];
-	url: string;
+  subject: string;
+  recipients: string[];
+  url: string;
 }
 
 export const sendForgotPasswordMail = async ({
-	subject,
-	recipients,
-	url,
+  subject,
+  recipients,
+  url,
 }: ForgotPasswordEmailProps) => {
-	return Effect.runPromise(
-		Effect.gen(function* (_) {
-			const { data, error } = yield* _(
-				Effect.tryPromise({
-					try: () =>
-						resend.emails.send({
-							from,
-							to: recipients,
-							subject,
-							react: ForgotPasswordEmail({ url }),
-						}),
-					catch: (e) => e,
-				}),
-			);
+  return Effect.runPromise(
+    Effect.gen(function* (_) {
+      const { data, error } = yield* _(
+        Effect.tryPromise({
+          try: () =>
+            resend.emails.send({
+              from,
+              to: recipients,
+              subject,
+              react: ForgotPasswordEmail({ url }),
+            }),
+          catch: (e) => e,
+        }),
+      );
 
-			if (error) {
-				return yield* _(Effect.fail(error));
-			}
+      if (error) {
+        return yield* _(Effect.fail(error));
+      }
 
-			return data;
-		}),
-	);
+      return data;
+    }),
+  );
 };
 
 export const sendVerificationEmail = async ({
-	subject,
-	recipients,
-	url,
+  subject,
+  recipients,
+  url,
 }: ForgotPasswordEmailProps) => {
-	return Effect.runPromise(
-		Effect.gen(function* (_) {
-			const { data, error } = yield* _(
-				Effect.tryPromise({
-					try: () =>
-						resend.emails.send({
-							from,
-							to: recipients,
-							subject,
-							react: VerificationEmail({ url }),
-						}),
-					catch: (e) => e,
-				}),
-			);
+  return Effect.runPromise(
+    Effect.gen(function* (_) {
+      const { data, error } = yield* _(
+        Effect.tryPromise({
+          try: () =>
+            resend.emails.send({
+              from,
+              to: recipients,
+              subject,
+              react: VerificationEmail({ url }),
+            }),
+          catch: (e) => e,
+        }),
+      );
 
-			if (error) {
-				return yield* _(Effect.fail(error));
-			}
+      if (error) {
+        return yield* _(Effect.fail(error));
+      }
 
-			return data;
-		}),
-	);
+      return data;
+    }),
+  );
 };
 
 interface SendMailProps {
-	subject: string;
-	recipients: string[];
-	react?: JSX.Element;
+  subject: string;
+  recipients: string[];
+  react?: JSX.Element;
 }
 
 export const sendMail = async ({
-	subject,
-	recipients,
-	react,
+  subject,
+  recipients,
+  react,
 }: SendMailProps) => {
-	return Effect.runPromise(
-		Effect.gen(function* (_) {
-			const { data, error } = yield* _(
-				Effect.tryPromise({
-					try: () =>
-						resend.emails.send({
-							from,
-							to: recipients,
-							subject,
-							react,
-						}),
-					catch: (e) => e,
-				}),
-			);
+  return Effect.runPromise(
+    Effect.gen(function* (_) {
+      const { data, error } = yield* _(
+        Effect.tryPromise({
+          try: () =>
+            resend.emails.send({
+              from,
+              to: recipients,
+              subject,
+              react,
+            }),
+          catch: (e) => e,
+        }),
+      );
 
-			if (error) {
-				return yield* _(Effect.fail(error));
-			}
+      if (error) {
+        return yield* _(Effect.fail(error));
+      }
 
-			return data;
-		}),
-	);
+      return data;
+    }),
+  );
 };
 
 export const sendMagicLinkEmail = async ({
-	subject,
-	recipients,
-	url,
+  subject,
+  recipients,
+  url,
 }: {
-	subject: string;
-	recipients: string[];
-	url: string;
+  subject: string;
+  recipients: string[];
+  url: string;
 }) => {
-	return Effect.runPromise(
-		Effect.gen(function* (_) {
-			const { data, error } = yield* _(
-				Effect.tryPromise({
-					try: () =>
-						resend.emails.send({
-							from,
-							to: recipients,
-							subject,
-							react: MagicLinkEmail({ url }),
-						}),
-					catch: (e) => e,
-				}),
-			);
+  return Effect.runPromise(
+    Effect.gen(function* (_) {
+      const { data, error } = yield* _(
+        Effect.tryPromise({
+          try: () =>
+            resend.emails.send({
+              from,
+              to: recipients,
+              subject,
+              react: MagicLinkEmail({ url }),
+            }),
+          catch: (e) => e,
+        }),
+      );
 
-			if (error) {
-				return yield* _(Effect.fail(error));
-			}
+      if (error) {
+        return yield* _(Effect.fail(error));
+      }
 
-			return data;
-		}),
-	);
+      return data;
+    }),
+  );
 };
--- core/src/zap/types/zap.config.types.ts
+++ core/src/zap/types/zap.config.types.ts
@@ -1,52 +1,52 @@
 export interface ZapSettings {
-	AI: {
-		SYSTEM_PROMPT: string;
-		CHAT?: {
-			MAX_TOKENS?: number;
-			TEMPERATURE?: number;
-			PRESENCE_PENALTY?: number;
-			FREQUENCY_PENALTY?: number;
-			STOP_SEQUENCES?: string[];
-			MAX_RETRIES?: number;
-		};
-		COMPLETION?: {
-			MAX_TOKENS?: number;
-			TEMPERATURE?: number;
-			PRESENCE_PENALTY?: number;
-			FREQUENCY_PENALTY?: number;
-			STOP_SEQUENCES?: string[];
-			MAX_RETRIES?: number;
-		};
-	};
-	AUTH: {
-		REQUIRE_EMAIL_VERIFICATION: boolean;
-		ENABLE_SOCIAL_PROVIDER: boolean;
-		MINIMUM_USERNAME_LENGTH: number;
-		MAXIMUM_USERNAME_LENGTH: number;
-		MINIMUM_PASSWORD_LENGTH: number;
-		MAXIMUM_PASSWORD_LENGTH: number;
-		REDIRECT_URL_AFTER_SIGN_UP: string;
-		REDIRECT_URL_AFTER_SIGN_IN: string;
-	};
-	NOTIFICATIONS: {
-		VAPID_MAIL: string;
-	};
-	MAIL: {
-		PREFIX: string;
-		RATE_LIMIT_SECONDS: number;
-		FROM: string;
-	};
-	PWA: {
-		NAME: string;
-		SHORT_NAME: string;
-		DESCRIPTION: string;
-		START_URL: string;
-		BACKGROUND_COLOR: string;
-		THEME_COLOR: string;
-		ICONS: Array<{
-			src: string;
-			sizes: string;
-			type: string;
-		}>;
-	};
+  AI: {
+    SYSTEM_PROMPT: string;
+    CHAT?: {
+      MAX_TOKENS?: number;
+      TEMPERATURE?: number;
+      PRESENCE_PENALTY?: number;
+      FREQUENCY_PENALTY?: number;
+      STOP_SEQUENCES?: string[];
+      MAX_RETRIES?: number;
+    };
+    COMPLETION?: {
+      MAX_TOKENS?: number;
+      TEMPERATURE?: number;
+      PRESENCE_PENALTY?: number;
+      FREQUENCY_PENALTY?: number;
+      STOP_SEQUENCES?: string[];
+      MAX_RETRIES?: number;
+    };
+  };
+  AUTH: {
+    REQUIRE_EMAIL_VERIFICATION: boolean;
+    ENABLE_SOCIAL_PROVIDER: boolean;
+    MINIMUM_USERNAME_LENGTH: number;
+    MAXIMUM_USERNAME_LENGTH: number;
+    MINIMUM_PASSWORD_LENGTH: number;
+    MAXIMUM_PASSWORD_LENGTH: number;
+    REDIRECT_URL_AFTER_SIGN_UP: string;
+    REDIRECT_URL_AFTER_SIGN_IN: string;
+  };
+  NOTIFICATIONS: {
+    VAPID_MAIL: string;
+  };
+  MAIL: {
+    PREFIX: string;
+    RATE_LIMIT_SECONDS: number;
+    FROM: string;
+  };
+  PWA: {
+    NAME: string;
+    SHORT_NAME: string;
+    DESCRIPTION: string;
+    START_URL: string;
+    BACKGROUND_COLOR: string;
+    THEME_COLOR: string;
+    ICONS: Array<{
+      src: string;
+      sizes: string;
+      type: string;
+    }>;
+  };
 }
