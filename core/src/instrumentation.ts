2ffc12b590b0b799b09855036d7cf5ae44a473a8
Alexandre Trotel <trotelalexandre@proton.me>
2025-07-13 18:01:27 +0200

    refactor: improve conditional import structure in instrumentation.ts for clarity

--- core/src/instrumentation.ts
+++ core/src/instrumentation.ts
@@ -1,5 +1,12 @@
 import "server-only";
 
 export async function register() {
-  await import("./zap/lib/orpc/server");
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    // This import supports only Node.js runtime.
+    await import("./zap/lib/orpc/server");
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    // This import supports only edge runtime.
+  }
 }
