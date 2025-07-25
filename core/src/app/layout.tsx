ccb8784b43f681ca4b35f3738b386e1af223f976
Alexandre Trotel <trotelalexandre@proton.me>
2025-07-25 05:48:27 +0200

    chore: improve font organization inside codebase

--- core/src/app/layout.tsx
+++ core/src/app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 import { Toaster } from "@/components/ui/sonner";
+import { geist } from "@/fonts";
 import { VERCEL } from "@/lib/env.client";
-import { geist } from "@/lib/font";
 import { ZAP_DEFAULT_METADATA, ZAP_DEFAULT_SETTINGS } from "@/zap.config";
 import Providers from "@/zap/providers/providers";
 
@@ -38,7 +38,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geist.className} antialiased`}>
+      <body className={`${geist.variable} antialiased`}>
         <Providers
           ENABLE_POSTHOG={ZAP_DEFAULT_SETTINGS.ANALYTICS.ENABLE_POSTHOG}
         >
--- core/src/lib/font.ts
+++
@@ -1,6 +0,0 @@
-import { Geist } from "next/font/google";
-
-export const geist = Geist({
-  subsets: ["latin"],
-  display: "swap",
-});
