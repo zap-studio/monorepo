a8a119813688dfe7680fefdce34d20b6c2354f91
Alexandre Trotel <trotelalexandre@proton.me>
2025-08-06 23:05:08 +0200

    refactor: replace uniqueIndex with unique in database schema files

--- core/src/zap/db/schema/notifications.sql.ts
+++ core/src/zap/db/schema/notifications.sql.ts
@@ -1,14 +1,14 @@
-import { pgTable, text, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+import { pgTable, text, unique, uuid } from "drizzle-orm/pg-core";
 import type webpush from "web-push";
 
 import { user } from "@/db/schema";
 
-export const pushNotifications = pgTable("push_notifications", {
-  id: uuid("uuid").primaryKey().defaultRandom(),
-  subscription: text("jsonb").$type<webpush.PushSubscription>().notNull(),
-  userId: text("uuid").references(() => user.id, { onDelete: "cascade" }),
-});
-
-export const pushNotificationsUserIndex = uniqueIndex(
-  "push_notifications_user_idx",
-).on(pushNotifications.userId);
+export const pushNotifications = pgTable(
+  "push_notifications",
+  {
+    id: uuid("uuid").primaryKey().defaultRandom(),
+    subscription: text("jsonb").$type<webpush.PushSubscription>().notNull(),
+    userId: text("uuid").references(() => user.id, { onDelete: "cascade" }),
+  },
+  (t) => [unique("push_notifications_user_idx").on(t.userId)],
+);
