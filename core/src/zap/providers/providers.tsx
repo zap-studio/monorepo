466e8b4d43f4d69f25dd0cb4a7a0adcbf781f8cd
Alexandre Trotel <trotelalexandre@proton.me>
2025-08-08 08:43:36 +0200

    feat: implement QueryClient and serializer in query.ts; update Providers to use shared queryClient

--- core/src/zap/providers/providers.tsx
+++ core/src/zap/providers/providers.tsx
@@ -2,7 +2,7 @@
 import "client-only";
 
 import { ProgressProvider } from "@bprogress/next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import posthog from "posthog-js";
@@ -10,6 +10,7 @@ import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { useEffect } from "react";
 
 import { PUBLIC_ENV } from "@/lib/env.public";
+import { queryClient } from "@/lib/query";
 import { ThemeProvider } from "@/providers/theme.provider";
 import SuspendedPostHogPageView from "@/zap/components/analytics/posthog-page-view";
 
@@ -17,8 +18,6 @@ interface ProvidersProps {
   children: React.ReactNode;
   ENABLE_POSTHOG: boolean;
 }
-
-const queryClient = new QueryClient();
 
 export default function Providers({
   children,
