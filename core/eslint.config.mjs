988987cb925c6c033961d3e7a001c4a8b082434e
Alexandre Trotel <trotelalexandre@proton.me>
2025-08-08 08:07:16 +0200

    feat: integrate TanStack Query and ESLint plugin, replace SWR with QueryClientProvider

--- core/eslint.config.mjs
+++ core/eslint.config.mjs
@@ -1,4 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginQuery from '@tanstack/eslint-plugin-query';
 import drizzlePlugin from "eslint-plugin-drizzle";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
@@ -13,6 +14,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...pluginQuery.configs['flat/recommended'],
   {
     plugins: {
       drizzle: drizzlePlugin,
--- core/src/zap/providers/providers.tsx
+++ core/src/zap/providers/providers.tsx
@@ -2,11 +2,12 @@
 import "client-only";
 
 import { ProgressProvider } from "@bprogress/next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { useEffect } from "react";
-import { SWRConfig } from "swr";
 
 import { PUBLIC_ENV } from "@/lib/env.public";
 import { ThemeProvider } from "@/providers/theme.provider";
@@ -16,6 +17,8 @@ interface ProvidersProps {
   children: React.ReactNode;
   ENABLE_POSTHOG: boolean;
 }
+
+const queryClient = new QueryClient();
 
 export default function Providers({
   children,
@@ -34,7 +37,7 @@ export default function Providers({
   }, [ENABLE_POSTHOG]);
 
   return (
-    <SWRConfig value={{ provider: () => new Map() }}>
+    <QueryClientProvider client={queryClient}>
       <NuqsAdapter>
         <ThemeProvider
           attribute="class"
@@ -58,6 +61,7 @@ export default function Providers({
           </ProgressProvider>
         </ThemeProvider>
       </NuqsAdapter>
-    </SWRConfig>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
