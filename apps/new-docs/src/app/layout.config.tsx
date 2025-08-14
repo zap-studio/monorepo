ec548701ca4d5cb7666771146c8b0568fd5f5c65
Alexandre Trotel <trotelalexandre@proton.me>
2025-08-14 17:42:38 +0200

    refactor: remove outdated documentation and improve layout configuration

--- apps/new-docs/src/app/layout.config.tsx
+++ apps/new-docs/src/app/layout.config.tsx
@@ -11,25 +11,12 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <span className="font-bold text-base">Zap.ts ⚡️</span>
+        <span className="font-semibold text-base">Zap.ts ⚡️</span>
       </>
     ),
   },
   // see https://fumadocs.dev/docs/ui/navigation/links
   links: [
-    {
-      text: 'Best Practices',
-      url: '/docs/misc/best-practices',
-    },
-    {
-      text: 'Contributors',
-      url: '/contributors',
-    },
-    {
-      text: 'Discussions',
-      url: 'https://github.com/alexandretrotel/zap.ts/discussions',
-      external: true,
-    },
     {
       text: 'Demo',
       url: 'https://demo.zap-ts.alexandretrotel.org',
--- apps/new-docs/src/app/layout.tsx
+++ apps/new-docs/src/app/layout.tsx
@@ -1,16 +1,16 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { Geist } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-const inter = Inter({
+const inter = Geist({
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+    <html className={inter.className} lang="en" suppressHydrationWarning>
+      <body className="flex min-h-screen flex-col">
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
