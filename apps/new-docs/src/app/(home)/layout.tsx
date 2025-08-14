89e06b96158665c1413168264831d0aab517b852
Alexandre Trotel <trotelalexandre@proton.me>
2025-08-14 18:04:33 +0200

    refactor: enhance documentation and installation instructions, update layout components with navigation links

--- apps/new-docs/src/app/(home)/layout.tsx
+++ apps/new-docs/src/app/(home)/layout.tsx
@@ -1,7 +1,27 @@
-import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
+import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return (
+    <HomeLayout
+      {...baseOptions}
+      links={[
+        {
+          text: 'Documentation',
+          url: '/docs',
+        },
+        {
+          text: 'Discussions',
+          url: 'https://github.com/alexandretrotel/zap.ts/discussions',
+        },
+        {
+          text: 'Demo',
+          url: 'https://demo.zap-ts.alexandretrotel.org',
+        },
+      ]}
+    >
+      {children}
+    </HomeLayout>
+  );
 }
