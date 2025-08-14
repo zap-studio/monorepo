21f7a5e60b16288101d8900f24a6838c00eca962
Alexandre Trotel <trotelalexandre@proton.me>
2025-08-14 23:52:05 +0200

    refactor: update font import and apply antialiased class for improved layout styling

--- apps/new-docs/src/app/layout.tsx
+++ apps/new-docs/src/app/layout.tsx
@@ -1,15 +1,19 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Geist } from 'next/font/google';
+import { Geist as Font } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-const inter = Geist({
+const font = Font({
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html className={inter.className} lang="en" suppressHydrationWarning>
+    <html
+      className={`${font.className} antialiased`}
+      lang="en"
+      suppressHydrationWarning
+    >
       <body className="flex min-h-screen flex-col">
         <RootProvider>{children}</RootProvider>
       </body>
