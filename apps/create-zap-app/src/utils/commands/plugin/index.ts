6c144266075d243ed6333c9d5d27dfc1ef735c4c
Alexandre Trotel <trotelalexandre@proton.me>
2025-08-14 09:01:21 +0200

    refactor: remove plugin command and related utilities for cleaner codebase

--- apps/create-zap-app/src/utils/commands/plugin/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './list-plugins';
-export * from './list-scripts';
-export * from './manage-plugins';
-export * from './plugin-utils';
-export * from './run-script';
-export * from './validate-system';
