b205c4e7c74c7b7c8ff9df47af8240f003156787
Alexandre Trotel <trotelalexandre@proton.me>
2025-08-20 11:03:35 +0200

    feat: add @bprogress/next dependency and integrate AppProgressProvider in Provider component

--- apps/docs/src/app/provider.tsx
+++ apps/docs/src/app/provider.tsx
@@ -1,7 +1,8 @@
 'use client';
+import { AppProgressProvider as ProgressProvider } from '@bprogress/next';
 import { RootProvider } from 'fumadocs-ui/provider';
-import SearchDialog from '@/components/search';
 import type { ReactNode } from 'react';
+import SearchDialog from '@/components/search';
 
 export function Provider({ children }: { children: ReactNode }) {
   return (
@@ -10,7 +11,14 @@ export function Provider({ children }: { children: ReactNode }) {
         SearchDialog,
       }}
     >
-      {children}
+      <ProgressProvider
+        color="#efb100"
+        height="4px"
+        options={{ showSpinner: false }}
+        shallowRouting
+      >
+        {children}
+      </ProgressProvider>
     </RootProvider>
   );
 }
