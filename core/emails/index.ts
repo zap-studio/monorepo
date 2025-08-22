11749fab2b11da5aa93763171434426f88639ff4
Alexandre Trotel <trotelalexandre@proton.me>
2025-08-22 19:08:48 +0200

    fix: update biome ignore comment for clarity in email template preview

--- core/emails/index.ts
+++ core/emails/index.ts
@@ -1,3 +1,4 @@
+// biome-ignore-all lint/performance/noBarrelFile: This is necessary for React Email server to preview mail templates
 export { default as ForgotPasswordMail } from './forgot-password.mail';
 export { default as MagicLinkMail } from './magic-link.mail';
 export { default as VerificationMail } from './verification.mail';
