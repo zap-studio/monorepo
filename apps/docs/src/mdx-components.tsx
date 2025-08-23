bdcd9d9fdf5c116740a84ecc10e9ed1c2996ed5d
Alexandre Trotel <trotelalexandre@proton.me>
2025-08-23 23:49:04 +0200

    refactor: restructure imports and component definitions for clarity and organization

--- apps/docs/src/mdx-components.tsx
+++ apps/docs/src/mdx-components.tsx
@@ -1,8 +1,44 @@
-import * as FileComponents from 'fumadocs-ui/components/files';
-import * as TabsComponents from 'fumadocs-ui/components/tabs';
+import { File, Files, Folder } from 'fumadocs-ui/components/files';
+import {
+  Tab,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from 'fumadocs-ui/components/tabs';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
-import * as TableComponents from '@/components/Table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/Table';
+
+const FileComponents = {
+  File,
+  Files,
+  Folder,
+};
+
+const TabsComponents = {
+  Tab,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+};
+
+const TableComponents = {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+};
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
