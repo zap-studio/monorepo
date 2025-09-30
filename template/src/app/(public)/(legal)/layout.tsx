23b2df163f415ffad4553160284856c8ed8a6884
Alexandre Trotel <trotelalexandre@proton.me>
2025-09-30 22:34:15 +0200

    refactor: enhance _LegalLayout to accept and utilize plugins for auth and blog

--- template/src/app/(public)/(legal)/layout.tsx
+++ template/src/app/(public)/(legal)/layout.tsx
@@ -1,7 +1,11 @@
+import { getServerPlugin } from "@/lib/zap.server";
 import _LegalLayout, {
   type _LegalLayoutProps,
 } from "@/zap/legal/layouts/legal.layout";
 
+const auth = getServerPlugin("auth");
+const blog = getServerPlugin("blog");
+
 export default function LegalLayout({ children }: _LegalLayoutProps) {
-  return <_LegalLayout>{children}</_LegalLayout>;
+  return <_LegalLayout plugins={{ auth, blog }}>{children}</_LegalLayout>;
 }
--- template/zap/legal/layouts/legal.layout.tsx
+++ template/zap/legal/layouts/legal.layout.tsx
@@ -1,18 +1,22 @@
 import type React from "react";
-
+import type { ZapServerPluginInstance } from "@/lib/zap.server";
 import { Footer } from "@/zap/components/common/footer";
 import { Header } from "@/zap/components/common/header";
 
 export type _LegalLayoutProps = {
   children: React.ReactNode;
+  plugins: Partial<{
+    auth: ZapServerPluginInstance<"auth">;
+    blog: ZapServerPluginInstance<"blog">;
+  }>;
 };
 
-export default function _LegalLayout({ children }: _LegalLayoutProps) {
+export default function _LegalLayout({ children, plugins }: _LegalLayoutProps) {
   return (
     <div className="relative flex min-h-screen flex-col">
-      <Header />
+      <Header plugins={plugins} />
       <div className="mx-auto max-w-3xl px-4 py-12 md:py-24">{children}</div>
-      <Footer />
+      <Footer plugins={plugins} />
     </div>
   );
 }
