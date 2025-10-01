63bffe7d660b86bac465d9b2b269b73a818c53e8
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-01 23:14:13 +0200

    refactor: move blogConfig initialization inside BlogPage component

--- template/src/app/(public)/blog/page.tsx
+++ template/src/app/(public)/blog/page.tsx
@@ -4,7 +4,8 @@ import { _BlogPage, _metadata } from "@/zap/blog/pages/blog.page";
 
 export const metadata: Metadata = _metadata;
 
+const blogConfig = getServerPluginConfig("blog") ?? {};
+
 export default function BlogPage() {
-  const blogConfig = getServerPluginConfig("blog") ?? {};
   return <_BlogPage pluginConfigs={{ blog: blogConfig }} />;
 }
