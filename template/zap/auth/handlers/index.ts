cbf8ff5c6eef7bd6a3a465c683ec6932a24aad1f
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-01 08:34:14 +0200

    refactor: update withAuthenticatedApiHandler to utilize pluginConfigs for authentication

--- template/zap/auth/handlers/index.ts
+++ template/zap/auth/handlers/index.ts
@@ -1,15 +1,19 @@
 import { UnauthorizedError } from "@/zap/errors";
 import { createHandler } from "@/zap/errors/handlers";
 import type { HandlerFunction, HandlerOptions } from "@/zap/errors/utils";
+import type { AuthServerPluginConfig } from "@/zap/plugins/types/auth.plugin";
 import { isAuthenticatedService } from "../services";
 
 export function withAuthenticatedApiHandler<T extends unknown[], R>(
   handler: HandlerFunction<T, R>,
+  pluginConfigs: {
+    auth: Partial<AuthServerPluginConfig>;
+  },
   options: HandlerOptions = {}
 ) {
   return createHandler(
     async (...args: T): Promise<R> => {
-      const isAuthenticated = await isAuthenticatedService();
+      const isAuthenticated = await isAuthenticatedService(pluginConfigs);
 
       if (!isAuthenticated) {
         throw new UnauthorizedError("User not authenticated");
