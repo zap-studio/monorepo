fda5db025895d7d4094ef6bbf968a980b59e2e5f
Alexandre Trotel <trotelalexandre@proton.me>
2025-10-24 20:13:17 +0200

    chore: format code and update configuration files for consistency

--- apps/website/src/main.tsx
+++ apps/website/src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import './assets/fonts/Geist/Geist-Regular.ttf';
-import { ThemeProvider } from './components/theme.provider.tsx';
-import { Page } from './page.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./assets/fonts/Geist/Geist-Regular.ttf";
+import { ThemeProvider } from "./components/theme.provider.tsx";
+import { Page } from "./page.tsx";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <Page />
-      </ThemeProvider>
-    </StrictMode>
-  );
+	createRoot(rootElement).render(
+		<StrictMode>
+			<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+				<Page />
+			</ThemeProvider>
+		</StrictMode>,
+	);
 }
