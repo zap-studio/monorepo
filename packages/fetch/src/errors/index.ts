ad007eda80f2ecf9701e5a8a42843ad084ade873
Alexandre Trotel <trotelalexandre@proton.me>
2025-11-26 20:52:08 +0100

    feat: simplify FetchError constructor by removing unnecessary parameters

--- packages/fetch/src/errors/index.ts
+++ packages/fetch/src/errors/index.ts
@@ -1,18 +1,11 @@
 export class FetchError extends Error {
-  status: number;
-  statusText: string;
+  status: Response["status"];
   response: Response;
 
-  constructor(
-    message: string,
-    status: number,
-    statusText: string,
-    response: Response
-  ) {
+  constructor(message: string, response: Response) {
     super(message);
     this.name = "FetchError";
-    this.status = status;
-    this.statusText = statusText;
+    this.status = response.status;
     this.response = response;
   }
 }
