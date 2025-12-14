ec000e6d586e447e0f714a7e5e2c6247b27b5c32
Alexandre Trotel <trotelalexandre@proton.me>
2025-12-14 21:32:55 +0100

    chore: remove initial tests

--- packages/permit/tests/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { greeting } from "../src";
-
-describe("permit", () => {
-  it("should export greeting", () => {
-    expect(greeting).toBe("Hello, world!");
-  });
-});
