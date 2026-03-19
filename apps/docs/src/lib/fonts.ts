20b752346c4443d8eeadba0536d1e0f21e1a30ba
alexandretrotel <trotelalexandre@proton.me>
2026-03-19 08:15:44 -0400

    feat: add font imports and exports for variable and serif fonts

---
+++ apps/docs/src/lib/fonts.ts
@@ -0,0 +1,8 @@
+import "@fontsource-variable/inter/wght.css";
+import "@fontsource-variable/jetbrains-mono/wght.css";
+import "@fontsource/instrument-serif/400.css";
+import "@fontsource/instrument-serif/400-italic.css";
+
+export { default as interLatinWghtNormalUrl } from "@fontsource-variable/inter/files/inter-latin-wght-normal.woff2?url";
+export { default as instrumentSerifLatinItalicUrl } from "@fontsource/instrument-serif/files/instrument-serif-latin-400-italic.woff2?url";
+export { default as instrumentSerifLatinNormalUrl } from "@fontsource/instrument-serif/files/instrument-serif-latin-400-normal.woff2?url";
