86a1c14ff9b1ccb7d1e82255bfa62431a415a0c4
alexandretrotel <trotelalexandre@proton.me>
2026-03-19 10:28:58 -0400

    feat: add staticFunctionMiddleware to changelog functions for improved handling

--- apps/docs/src/lib/changelog/changelog.functions.ts
+++ apps/docs/src/lib/changelog/changelog.functions.ts
@@ -1,13 +1,15 @@
 import { createServerFn } from "@tanstack/react-start";
+import { staticFunctionMiddleware } from "@tanstack/start-static-server-functions";
 import {
   getPackageChangelogBySlug,
   getReleasedPackageChangelogs,
 } from "@/lib/changelog/changelog.server";
 
-export const getReleasedPackageChangelogsFn = createServerFn({ method: "GET" }).handler(() =>
-  getReleasedPackageChangelogs(),
-);
+export const getReleasedPackageChangelogsFn = createServerFn({ method: "GET" })
+  .middleware([staticFunctionMiddleware])
+  .handler(() => getReleasedPackageChangelogs());
 
 export const getPackageChangelogBySlugFn = createServerFn({ method: "GET" })
   .inputValidator((data: { slug: string }) => data)
+  .middleware([staticFunctionMiddleware])
   .handler(({ data }) => getPackageChangelogBySlug(data.slug));
