05656553f8a4e8321ce84444e90de8253066a8e6
alexandretrotel <trotelalexandre@proton.me>
2026-07-09 22:41:04 +0200

    chore: merge benchmarks and tests config

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -13,16 +13,8 @@ export default defineConfig({
   },
   overrides: [
     {
-      files: ["**/benchmarks/**/*.{ts,tsx}"],
-      rules: {
-        "typescript/no-unsafe-assignment": "off",
-        "typescript/no-unsafe-return": "off",
-        "typescript/no-unsafe-type-assertion": "off",
-        "unicorn/no-await-expression-member": "off",
-      },
-    },
-    {
       files: [
+        "**/benchmarks/**/*.{ts,tsx}",
         "**/*.test.{ts,tsx}",
         "**/*.spec.{ts,tsx}",
         "**/*.node.test.{ts,tsx}",
@@ -36,12 +28,14 @@ export default defineConfig({
         "typescript/no-unsafe-argument": "off",
         "typescript/no-unsafe-assignment": "off",
         "typescript/no-unsafe-member-access": "off",
+        "typescript/no-unsafe-return": "off",
         "typescript/no-unsafe-type-assertion": "off",
         "typescript/only-throw-error": "off",
         "typescript/promise-function-async": "off",
         "typescript/strict-void-return": "off",
         "typescript/unbound-method": "off",
         "unicorn/consistent-function-scoping": "off",
+        "unicorn/no-await-expression-member": "off",
         "unicorn/prefer-response-static-json": "off",
         "vitest/max-expects": "off",
       },
