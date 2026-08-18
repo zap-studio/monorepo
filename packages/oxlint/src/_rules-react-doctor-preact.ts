264b3a5949643e2373d39fd7119a41e6cd51de3c
alexandretrotel <trotelalexandre@proton.me>
2026-08-18 18:14:55 +0200

    feat: add preact rules

---
+++ packages/oxlint/src/_rules-react-doctor-preact.ts
@@ -0,0 +1,9 @@
+import type { RuleMap } from "./_prefixed.ts";
+
+export const reactDoctorPreactRules = {
+  "preact-no-children-length": "warn",
+  "preact-no-react-hooks-import": "warn",
+  "preact-no-render-arguments": "warn",
+  "preact-prefer-ondblclick": "warn",
+  "preact-prefer-oninput": "warn",
+} satisfies RuleMap;
