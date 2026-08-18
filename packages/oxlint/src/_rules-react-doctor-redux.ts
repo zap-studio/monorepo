93b925b3d441f543fc3615a0cb58480dd2def695
alexandretrotel <trotelalexandre@proton.me>
2026-08-18 18:13:58 +0200

    feat: add redux rules

---
+++ packages/oxlint/src/_rules-react-doctor-redux.ts
@@ -0,0 +1,6 @@
+import type { RuleMap } from "./_prefixed.ts";
+
+export const reactDoctorReduxRules = {
+  "redux-useselector-inline-derivation": "warn",
+  "redux-useselector-returns-new-collection": "warn",
+} satisfies RuleMap;
