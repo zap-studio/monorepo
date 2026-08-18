2db16c9a060fcb88b75c3ba0338a3a2efab2150a
alexandretrotel <trotelalexandre@proton.me>
2026-08-18 17:39:11 +0200

    fix: rework oxfmt config

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,17 +1,4 @@
+import zapStudio from "@zap-studio/oxfmt/base";
 import { defineConfig } from "oxfmt";
 
-export default defineConfig({
-  sortImports: {
-    groups: [
-      "type-import",
-      ["value-builtin", "value-external"],
-      "type-internal",
-      "value-internal",
-      ["type-parent", "type-sibling", "type-index"],
-      ["value-parent", "value-sibling", "value-index"],
-      "unknown",
-    ],
-    newlinesBetween: true,
-  },
-  sortPackageJson: true,
-});
+export default defineConfig(zapStudio);
