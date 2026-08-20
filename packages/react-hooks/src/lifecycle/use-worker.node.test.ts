73e947ad5826f1151e03c31f446718ff73bf5391
alexandretrotel <trotelalexandre@proton.me>
2026-08-20 15:52:48 +0200

    feat: add useWorker hook

---
+++ packages/react-hooks/src/lifecycle/use-worker.node.test.ts
@@ -0,0 +1,18 @@
+import { createElement } from "react";
+import { renderToString } from "react-dom/server";
+import { describe, expect, it } from "vitest";
+
+import { useWorker } from "./use-worker.ts";
+
+function TestComponent() {
+  const { supported } = useWorker(() => ({}) as Worker);
+  return supported ? "true" : "false";
+}
+
+describe(useWorker, () => {
+  it("renders false on the server, before Worker can be read", () => {
+    const html = renderToString(createElement(TestComponent));
+
+    expect(html).toBe("false");
+  });
+});
