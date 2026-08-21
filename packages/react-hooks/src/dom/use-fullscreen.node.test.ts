a7c2b87d30ec1f8138bda77409109c4300edfd8b
alexandretrotel <trotelalexandre@proton.me>
2026-08-21 13:18:45 +0200

    feat: add useFullscreen hook

---
+++ packages/react-hooks/src/dom/use-fullscreen.node.test.ts
@@ -0,0 +1,18 @@
+import { createElement } from "react";
+import { renderToString } from "react-dom/server";
+import { describe, expect, it } from "vitest";
+
+import { useFullscreen } from "./use-fullscreen.ts";
+
+function TestComponent() {
+  const { isFullscreen, ref } = useFullscreen<HTMLDivElement>();
+  return createElement("div", { ref }, isFullscreen ? "true" : "false");
+}
+
+describe(useFullscreen, () => {
+  it("renders false on the server, before any fullscreenchange can fire", () => {
+    const html = renderToString(createElement(TestComponent));
+
+    expect(html).toBe("<div>false</div>");
+  });
+});
