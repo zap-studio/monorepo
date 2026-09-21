7fea8b32d7266b17a91815fe333af07db8f51670
alexandretrotel <trotelalexandre@proton.me>
2026-09-21 14:05:55 +0200

    fix: remove fonts

--- apps/docs/blume.config.ts
+++ apps/docs/blume.config.ts
@@ -80,7 +80,6 @@ export default defineConfig({
   },
   theme: {
     accent: { dark: "hsl(43, 96%, 56%)", light: "hsl(38, 92%, 40%)" },
-    fonts: { display: "space-grotesk", mono: "ibm-plex-mono" },
     radius: "none",
     mode: "system",
   },
